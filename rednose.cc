23915134bfd49b7807541e52661d2bcd395ef7f7
Cosmo Steiner-Schlede <106993529+b4k4kozmo@users.noreply.github.com>
2024-03-14 16:06:54 -0700

    added error checks for non digits

--- rednose.cc
+++ rednose.cc
@@ -7,9 +7,19 @@ void setValues()
 {
     std::cout<<"Enter the age you want to check the perception of"<<std::endl;
     std::cin>>yA;
+    while (std::cin.fail()){
+        std::cin.clear();
+        std::cout<<"Enter the age you want to check the perception of"<<std::endl;
+        std::cin>>yA;
+    }
     std::cout<<std::endl;
     std::cout<<"Enter the age you want to compare it to"<<std::endl;
     std::cin>>xA;
+    while (std::cin.fail()){
+        std::cin.clear();
+        std::cout<<"Enter the age you want to compare it to"<<std::endl;
+        std::cin>>xA;
+    }
     std::cout<<std::endl;
     std::cout<<"Type a unit of time"<<std::endl;
     std::cin>>units;
@@ -17,6 +27,13 @@ void setValues()
     std::cout<<"Enter an amount of ";
     std::cout<<units<<std::endl;
     std::cin>>t;
+    while (std::cin.fail()){
+        std::cin.clear();
+        std::cin.ignore();
+        std::cout<<"Enter an amount of ";
+        std::cout<<units<<std::endl;
+        std::cin>>t;
+    }
     std::cout<<std::endl;
 }
 float calculateP()
